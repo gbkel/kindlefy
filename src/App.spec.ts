ccbaf079639e3a657851513f9d22b37aa8c619c2
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2021-11-27 13:52:23 -0300

    chore(*): remove test files

--- src/App.spec.ts
+++
@@ -1,7 +0,0 @@
-import { App } from "@/App"
-
-describe("Unit Test", () => {
-	test("Should do something", () => {
-		App.start()
-	})
-})
--- src/App.test.ts
+++
@@ -1,7 +0,0 @@
-import { App } from "@/App"
-
-describe("Integration Test", () => {
-	test("Should do something", () => {
-		App.start()
-	})
-})
