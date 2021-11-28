398cf111dbc090e729fddc62bc147957eb9f47dd
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2021-11-28 10:54:09 -0300

    chore(*): add built file

--- setup.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-
-apt-get install -y python wget gcc xz-utils imagemagick xdg-utils build-essential curl git-core
-
-wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
