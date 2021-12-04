a998d6c8894c55ad9066f30ba9b86bee9caa07cc
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2021-12-04 11:13:22 -0300

    chore(*): add test setup

---
+++ jest-integration-config.js
@@ -0,0 +1,7 @@
+const config = require("./jest.config")
+
+config.testMatch = [
+	"**/*.test.ts"
+]
+
+module.exports = config
---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils")
+const path = require("path")
+
+const { compilerOptions } = require("./tsconfig.json")
+
+module.exports = {
+	roots: [
+		"<rootDir>/src"
+	],
+	moduleNameMapper: pathsToModuleNameMapper(
+		compilerOptions.paths,
+		{
+			prefix: `${path.resolve(__dirname, ".")}/`
+		}
+	),
+	collectCoverageFrom: [
+		"<rootDir>/src/**/*.ts"
+	],
+	coverageDirectory: "coverage",
+	testEnvironment: "node",
+	transform: {
+		".+\\.ts$": "ts-jest"
+	}
+}
