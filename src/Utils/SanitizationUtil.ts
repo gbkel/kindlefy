f46c70eaa75d49a05beba4dba3b1fcb782e87530
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-01-26 20:07:57 -0300

    feat(sanitization): remove spaces

--- src/Utils/SanitizationUtil.ts
+++ src/Utils/SanitizationUtil.ts
@@ -1,6 +1,6 @@
 class SanitizationUtil {
 	sanitizeFilename (rawFilename: string): string {
-		return rawFilename.replace(/[^a-zA-Z0-9 \.]/g, " ")
+		return rawFilename.replace(/[^a-zA-Z0-9 \.]/g, "")
 	}
 }
 
