5eb021f5b017b6dab812f0582f6dee80d86395fe
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-03-03 00:11:27 -0300

    feat(http): remove user agent

--- src/Services/HttpService.ts
+++ src/Services/HttpService.ts
@@ -7,10 +7,7 @@ class HttpService {
 
 	constructor (options: HttpOptions) {
 		this.client = axios.create({
-			baseURL: options.baseURL,
-			headers: {
-				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
-			}
+			baseURL: options.baseURL
 		})
 	}
 
