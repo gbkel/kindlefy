ead54c15004810abb0d66a74c054c6868fef48fa
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-03-06 21:30:13 -0300

    chore(http-service): use more verbose variable

--- src/Services/HttpProxyService.integration.test.ts
+++ src/Services/HttpProxyService.integration.test.ts
@@ -3,9 +3,9 @@ import HttpProxyService from "@/Services/HttpProxyService"
 describe("HttpProxyService", () => {
 	describe("get()", () => {
 		test("Should retrieve a HTML page content", async () => {
-			const postHTML = await HttpProxyService.get("https://google.com")
+			const pageHTML = await HttpProxyService.get("https://google.com")
 
-			expect(postHTML).toBeTruthy()
+			expect(pageHTML).toBeTruthy()
 		})
 	})
 })
